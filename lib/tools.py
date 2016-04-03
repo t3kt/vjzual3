dc5bad08ebcaceec5d1184d113449dcbaa92eae9
t3kt <tommy.etkin@gmail.com>
2016-04-02 23:56:57 -0700

    adding more editor tool actions

--- lib/tools.py
+++ lib/tools.py
@@ -1,3 +1,4 @@
+from numpy import interp
 if False:
 	from _stubs import *
 
@@ -78,6 +79,30 @@ def _saveTox(comp):
 def SaveToxSelectedOrContext():
 	_doOnSelectedOrContext(_saveTox)
 
+_orderAxes = {
+	'x': {'attr': 'nodeX', 'reverse': False},
+	'y': {'attr': 'nodeY', 'reverse': True},
+}
+
+def _setAlignOrderBy(sortAttrName, reverseDir):
+	selected = _getSelected()
+	n = len(selected)
+	selected = sorted(
+		selected,
+		key=lambda o: getattr(o, sortAttrName),
+		reverse=reverseDir)
+	for i in range(n):
+		if hasattr(selected[i].par, 'order'):
+			val = interp(float(i), [0, n - 1], [0.0, 1.0])
+			selected[i].par.order = val
+
+def SetAlignOrderBy(axis):
+	_setAlignOrderBy(_orderAxes[axis]['attr'], _orderAxes[axis]['reverse'])
+
+def CopySelectedPaths():
+	sel = _getSelected()
+	ui.clipboard = ' '.join([o.path for o in sel])
+
 def ReloadDATs(dats):
 	for dat in dats:
 		if not dat.isDAT or not hasattr(dat.par, 'reload'):
@@ -86,6 +111,139 @@ def ReloadDATs(dats):
 			print('Reloading DAT: ' + dat.path)
 			dat.par.reload.pulse(1)
 
+def DestroyPars(parnames):
+	selected = _getSelected()
+	if len(parnames) == 1 and parnames[0] == '*':
+		print('destroy all pars', selected)
+		for o in selected:
+			while len(o.customPages) > 0:
+				o.customPages.pop().destroy()
+	else:
+		print('destroyPars', parnames, selected)
+		for o in selected:
+			oPars = o.pars(*parnames)
+			for p in oPars:
+				if p.isCustom:
+					p.destroy()
+
+def AddTags(tags):
+	tags = set(tags)
+	selected = _getSelected()
+	print('addTags', tags, selected)
+	for o in selected:
+		o.tags |= tags
+
+def RemoveTags(tags):
+	if len(tags) == 1 and tags[0] == '*':
+		tags = None
+	else:
+		tags = set(tags)
+	selected = _getSelected()
+	print('removeTags', tags if tags else '*', selected)
+	for o in selected:
+		if tags:
+			o.tags -= tags
+		else:
+			o.tags.clear()
+
+def _getMiddle(vals):
+	low, high = min(vals), max(vals)
+	return low + (high - low) / 2
+
+class NodeEdge:
+	def __init__(self, attrName, calc):
+		self.attrName = attrName
+		self.calc = calc
+	def get(self, o):
+		return getattr(o, self.attrName)
+	def set(self, o, val):
+		setattr(o, self.attrName, val)
+	def align(self, os):
+		vals = [self.get(o) for o in os]
+		newval = self.calc(vals)
+		for o in os:
+			self.set(o, newval)
+
+class DerivedNodeEdge(NodeEdge):
+	def __init__(self, baseAttr, sizeAttr, calc):
+		NodeEdge.__init__(self, baseAttr, calc)
+		self.sizeAttr = sizeAttr
+	def get(self, o):
+		return getattr(o, self.attrName) + getattr(o, self.sizeAttr)
+	def set(self, o, val):
+		setattr(o, self.attrName, val - getattr(o, self.sizeAttr))
+
+nodeEdges = {
+	'left': NodeEdge('nodeX', min),
+	'top': NodeEdge('nodeY', max),
+	'center': NodeEdge('nodeCenterX', _getMiddle),
+	'middle': NodeEdge('nodeCenterY', _getMiddle),
+	'right': DerivedNodeEdge('nodeX', 'nodeWidth', max),
+	'bottom': DerivedNodeEdge('nodeY', 'nodeHeight', min)
+}
+
+def Align(dirName):
+	selected = _getSelected()
+	if len(selected) < 2:
+		return
+	edge = nodeEdges[dirName]
+	edge.align(selected)
+
+def Distribute(axis):
+	if axis == 'x':
+		attr = 'nodeX'
+	else:
+		attr = 'nodeY'
+	selected = _getSelected()
+	n = len(selected)
+	if n < 3:
+		return
+	vals = [getattr(o, attr) for o in selected]
+	minVal, maxVal = min(vals), max(vals)
+	selected = sorted(
+		selected,
+		key=lambda o: getattr(o, attr))
+	for i in range(n):
+		val = interp(float(i), [0, n - 1], [minVal, maxVal])
+		setattr(selected[i], attr, round(val))
+
+def SortByName(axis):
+	attr = _orderAxes[axis]['attr']
+	reverse = _orderAxes[axis]['reverse']
+	selected = _getSelected()
+	n = len(selected)
+	if n < 2:
+		return
+	vals = [getattr(o, attr) for o in selected]
+	minVal, maxVal = min(vals), max(vals)
+	selected = sorted(
+		selected,
+		key=lambda o: o.name,
+		reverse=reverse)
+	for i in range(n):
+		val = interp(float(i), [0, n - 1], [minVal, maxVal])
+		setattr(selected[i], attr, round(val))
+
 class ToolsExt:
 	def __init__(self, comp):
 		self.comp = comp
+
+	def PerformAction(self, cmd):
+		if cmd.startswith('Align'):
+			Align(cmd.replace('Align', ''))
+		elif cmd.startswith('Orderby'):
+			SetAlignOrderBy(cmd.replace('Orderby', ''))
+		elif cmd.startswith('Distribute'):
+			Distribute(cmd.replace('Distribute', ''))
+		elif cmd.startswith('Sort'):
+			SortByName(cmd.replace('Sort', ''))
+		elif cmd == 'Initselected':
+			InitSelectedOrContext()
+		elif cmd == 'Reloadcode':
+			patterns = self.comp.par.Codeops.eval().split(' ')
+			ReloadDATs(ops(*patterns))
+		elif cmd == 'Reloadconfig':
+			patterns = self.comp.par.Configops.eval().split(' ')
+			ReloadDATs(ops(*patterns))
+		else:
+			raise Exception('unrecognized action: ' + cmd)
