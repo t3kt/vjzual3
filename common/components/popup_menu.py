f527da521d3e28ff193fcae0d191efc92616e0ff
t3kt <tommy.etkin@gmail.com>
2016-11-14 23:51:19 -0800

    adding a centralized popup menu component. this will make it possible to reduce the number of cloned menu components. not completed yet.

---
+++ common/components/popup_menu.py
@@ -0,0 +1,27 @@
+print('common/popup_menu.py initializing')
+
+try:
+	import common_base as base
+except ImportError:
+	try:
+		import base
+	except ImportError:
+		import common.lib.base as base
+
+import json
+
+class PopupMenu(base.Extension):
+	def __init__(self, comp):
+		super().__init__(comp)
+
+	def BuildMenuItems(self, dat, defaultitemsjson):
+		itemsjson = self.comp.par.Menuitemsjson.eval() or defaultitemsjson
+		items = json.loads(itemsjson)
+		dat.clear()
+		dat.appendRow(['text', 'action', 'help'])
+		for item in items:
+			dat.appendRow([
+				item.get('text', ''),
+				item.get('action', ''),
+				item.get('help', ''),
+			])
