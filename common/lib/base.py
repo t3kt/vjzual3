9297b2b8600149c45a21f367ec3a5dd42d2cef8d
t3kt <tommy.etkin@gmail.com>
2016-11-18 23:42:14 -0800

    ported over some logging improvements to the base extension class

--- common/lib/base.py
+++ common/lib/base.py
@@ -5,6 +5,35 @@ try:
 except ImportError:
 	import util
 
+class IndentedLogger:
+	def __init__(self):
+		self._indentLevel = 0
+		self._indentStr = ''
+
+	def _AddIndent(self, amount):
+		self._indentLevel += amount
+		self._indentStr = '\t' * self._indentLevel
+
+	def Indent(self):
+		self._AddIndent(1)
+
+	def Unindent(self):
+		self._AddIndent(-1)
+
+	def LogEvent(self, path, opid, event):
+		util.Log('%s [%s] %s (%s)' % (self._indentStr, opid or '', event, path or ''))
+
+	def LogBegin(self, path, opid, event):
+		self.LogEvent(path, opid, event)
+		self.Indent()
+
+	def LogEnd(self, path, opid, event):
+		self.Unindent()
+		if event:
+			self.LogEvent(path, opid, event)
+
+logger = IndentedLogger()
+
 class Extension:
 	def __init__(self, comp):
 		self.comp = comp
@@ -18,4 +47,12 @@ class Extension:
 		return master.path if master else self.comp.path
 
 	def _LogEvent(self, event):
-		util.Log('%s [%s] %s' % (self.comp.path, self._GetId() or '', event))
+		logger.LogEvent(self.comp.path, self._GetId(), event)
+
+	def _LogBegin(self, event):
+		logger.LogBegin(self.comp.path, self._GetId(), event)
+
+	def _LogEnd(self, event=None):
+		logger.LogEnd(self.comp.path, self._GetId(), event)
+
+
