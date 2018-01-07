9b2192dc11ea885e4008a42f6ba24bfd7f0ae8f1
tekt <tommy.etkin@gmail.com>
2018-01-06 19:53:23 -0800

    making module stub a bit more forgiving

--- lib/module_stub.py
+++ lib/module_stub.py
@@ -13,14 +13,28 @@ def CreateModule(comp):
 		return ModuleStub(comp)
 
 class ModuleStub:
-	def __init__(self, comp):
+	def __init__(self, comp, realmod=None):
 		self.comp = comp
+		self._realmod = realmod
 
 	@property
-	def IsModuleStub(self): return True
+	def realmod(self):
+		if self._realmod:
+			return self._realmod
+		elif IsHosted():
+			modtype = mod.shell_module.Module
+			for ext in self.comp.extensions:
+				if isinstance(ext, modtype):
+					self._realmod = ext
+					break
+		return self._realmod
 
 	@property
-	def HasApp(self): return False
+	def IsModuleStub(self): return self.realmod is None
+
+	@property
+	def HasApp(self):
+		return self.realmod.HasApp if self.realmod else False
 
 	def _NotSupported(self, action, iserror=False):
 		message = 'Unsupported action: {}'.format(action)
@@ -28,31 +42,58 @@ class ModuleStub:
 		if iserror:
 			raise Exception(message)
 
-	def ResetState(self): self._NotSupported('ResetState')
+	def ResetState(self):
+		if self.realmod:
+			self.realmod.ResetState()
+		else:
+			self._NotSupported('ResetState')
 
-	def UpdateHeight(self): self._NotSupported('UpdateHeight')
+	def UpdateHeight(self):
+		if self.realmod:
+			self.realmod.UpdateHeight()
+		else:
+			self._NotSupported('UpdateHeight')
 
-	def GetSchema(self, **_): self._NotSupported('GetSchema', iserror=True)
+	def GetSchema(self, **kwargs):
+		if self.realmod:
+			return self.realmod.GetSchema(**kwargs)
+		else:
+			self._NotSupported('GetSchema', iserror=True)
 
-	def UpdateSolo(self): self._NotSupported('UpdateSolo')
+	def UpdateSolo(self):
+		if self.realmod:
+			self.realmod.UpdateSolo()
+		else:
+			self._NotSupported('UpdateSolo')
 
-	def BuildDefaultParameterMetadata(self, *_): self._NotSupported('BuildDefaultParameterMetadata')
+	def BuildDefaultParameterMetadata(self, *args):
+		if self.realmod:
+			self.realmod.BuildDefaultParameterMetadata(*args)
+		else:
+			self._NotSupported('BuildDefaultParameterMetadata')
 
-	def GetParameterMetadata(self, parname): self._NotSupported('GetParameterMetadata({0!r})'.format(parname))
+	def GetParameterMetadata(self, parname):
+		if self.realmod:
+			return self.realmod.GetParameterMetadata(parname)
+		else:
+			self._NotSupported('GetParameterMetadata({0!r})'.format(parname))
 
 	def GetParamsWithFlag(self, flag, **kwargs):
-		self._NotSupported('GetParamsWithFlag({0!r}, {1!r})'.format(flag, kwargs))
-		return []
+		if self.realmod:
+			return self.realmod.GetParamsWithFlag(flag, **kwargs)
+		else:
+			self._NotSupported('GetParamsWithFlag({0!r}, {1!r})'.format(flag, kwargs))
+			return []
 
 	@property
-	def SubModuleOpNames(self): return []
+	def SubModuleOpNames(self): return self.realmod.SubModuleOpNames if self.realmod else []
 
 	@property
 	def SelectorOpNames(self):
 		return [s.name for s in self.comp.findChildren(depth=1, parName='clone', parValue='*_selector')]
 
 	@property
-	def ExposedModParamNames(self): return []
+	def ExposedModParamNames(self): return self.realmod.ExposedModParamNames if self.realmod else []
 
 def ParseStringList(val):
 	if not val:
